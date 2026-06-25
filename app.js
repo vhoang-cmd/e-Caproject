5bc020cc1a34b17e836af8f7b8e5293f173d7ee7
Hoang Long Viet <v_hoang@visionet.local>
2026-06-25 16:14:33 +0900

    update code , anh nen cac thu

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const passwordInput = document.getElementById('password');
 const strengthBar = document.getElementById('strength-bar');
 const strengthText = document.getElementById('strength-text');
 const togglePassword = document.querySelector('#togglePassword');
-const password = document.querySelector('#password');
 
-togglePassword.addEventListener('click', function (e) {
-    // Thay đổi kiểu từ password sang text và ngược lại
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    
-    // Thay đổi icon con mắt (nếu bạn muốn)
-    this.innerText = type === 'password' ? '👨‍🦯' : '🙈';
+
+
+togglePassword.addEventListener('click', function () {
+
+    const type = passwordInput.getAttribute('type') === 'password'
+        ? 'text'
+        : 'password';
+
+    passwordInput.setAttribute('type', type);
+
+    this.innerText = type === 'password' ? '👁️' : '🙈';
 });
 
 passwordInput.addEventListener('input', () => {
@@ -18,24 +21,24 @@ passwordInput.addEventListener('input', () => {
     let strength = 0;
 
     // Logic kiểm tra đơn giản
-    if (val.length > 5) strength += 33;
-    if (val.match(/[A-Z]/) && val.match(/[0-9]/)) strength += 33;
+    if (val.length >= 4) strength += 40;
+    if (val.match(/[A-Z]/) && val.match(/[0-9]/)) strength += 30;
     if (val.length > 10) strength += 34;
 
-    // Cập nhật giao diện
+    
     strengthBar.style.width = strength + '%';
 
-    if (strength < 40) {
+    if (strength <= 40) {
         strengthBar.style.backgroundColor = '#ff4d4d'; // Đỏ (Yếu)
-        strengthText.innerText = '弱い (Yếu)';
+        strengthText.innerText = '弱い';
         strengthText.style.color = '#ff4d4d';
-    } else if (strength < 80) {
+    } else if (strength < 75) {
         strengthBar.style.backgroundColor = '#ffd700'; // Vàng (Trung bình)
-        strengthText.innerText = '普通 (Bình thường)';
+        strengthText.innerText = '普通 ';
         strengthText.style.color = '#ffd700';
     } else {
         strengthBar.style.backgroundColor = '#2ecc71'; // Xanh lá (Mạnh - Đúng ý bạn)
-        strengthText.innerText = '強い (Mạnh)';
+        strengthText.innerText = '強い';
         strengthText.style.color = '#2ecc71';
     }
 });
